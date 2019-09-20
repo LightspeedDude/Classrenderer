d0f5e0e7797d297a4c3e8e569d1f0e9355f9ab6c
LightspeedDude <julio_martinez25@live.com>
2019-09-19 22:02:04 -0400

    Looks good

--- Classrenderer/Classrenderer.Android/Effects/BorderlessEffectDroid.cs
+++ Classrenderer/Classrenderer.Android/Effects/BorderlessEffectDroid.cs
@@ -9,16 +9,33 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Classrenderer.Droid.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(BorderlessEffectDroid), "BorderLessEffect")]
+
 
 namespace Classrenderer.Droid.Effects
 {
     public class BorderlessEffectDroid: PlatformEffect
     {
-
+        Android.Graphics.Color originalBackgroundColor = new Android.Graphics.Color(0, 0, 0, 0);
+        Android.Graphics.Color backgroundColor;
         protected override void OnAttached()
         {
+            //Control.SetBackground(null);
+
+            try
+            {
+                backgroundColor = Android.Graphics.Color.LightGreen;
+                Control.SetBackgroundColor(backgroundColor);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
 
         }
 
@@ -26,5 +43,28 @@ namespace Classrenderer.Droid.Effects
         {
 
         }
-    }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (((Android.Graphics.Drawables.ColorDrawable)Control.Background).Color == backgroundColor)
+                    {
+                        Control.SetBackgroundColor(originalBackgroundColor);
+                    }
+                    else
+                    {
+                        Control.SetBackgroundColor(backgroundColor);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+
+        }
 }
--- Classrenderer/Classrenderer.iOS/Effects/BorderlessEffectIOS.cs
+++ Classrenderer/Classrenderer.iOS/Effects/BorderlessEffectIOS.cs
@@ -1,26 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
-
+using Classrenderer.iOS.Effects;
 using Foundation;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(BorderlessEffectIOS), "BorderLessEffect")]
+
 namespace Classrenderer.iOS.Effects
 {
     public class BorderlessEffectIOS: PlatformEffect
     {
-
+        UIColor backgroundColor;
         protected override void OnAttached()
         {
-            var control = Control as UITextField;
-        }
+            //var control = Control as UITextField;
+            //control.BorderStyle = UITextBorderStyle.None;
 
+            try
+            {
+                Control.BackgroundColor = backgroundColor= UIColor.FromRGB(204, 153, 255);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+
+        }
         protected override void OnDetached()
         {
             
         }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (Control.BackgroundColor == backgroundColor)
+                    {
+                        Control.BackgroundColor = UIColor.White;
+                    }
+                    else
+                    {
+                        Control.BackgroundColor = backgroundColor;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
     }
 }
